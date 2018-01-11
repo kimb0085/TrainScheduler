2e89cf315e9eedffb4271ab717945492b3a1a87b
kimb0085 <kathleen.kimble@gmail.com>
2018-01-11 16:47:16 -0600

    styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBXo6pbtAp25dRESdY5MznQrbS4eYKh_g4",
+    authDomain: "trainscheduler-bf858.firebaseapp.com",
+    databaseURL: "https://trainscheduler-bf858.firebaseio.com",
+    projectId: "trainscheduler-bf858",
+    storageBucket: "",
+    messagingSenderId: "939304117402"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var trainName = "";
+  var destination = "";
+  var frequency = "";
+  var startTime = "HH:mm:ss";
+  var nextArrival = "";
+  var minutesAway = "";
+
+  var newTime = moment(startTime).format("HH:mm");
+
+  $(document).ready(function() {
+  	$(".table").append([
+  		'<tr>',
+  			'<td>Winston</td>',
+  			'<td>London</td>',
+  			'<td>25</td>',
+  			'<td>17:32</td>',
+  			'<td>12</td>',
+
+  		'<tr>',
+  			'<td>Ruby</td>',
+  			'<td>Chicago</td>',
+  			'<td>2</td>',
+  			'<td>08:16</td>',
+  			'<td>1</td>'])
+  });
+
+  $("addData").on("click", function(event) {
+  	event.preventDefault();
+
+  	trainName = $("#trainName").val().trim();
+  	destination = $("#destination").val().trim();
+  	startTime = $("#startTime").val().trim();
+  	frequency = $("#frequency").val().trim();
+
+  	var newTrain = database.ref().push({
+  		trainName: trainName,
+  		destination: destination,
+  		startTime: startTime,
+  		frequency: frequency,
+  		dateAdded: firebase.database.ServerValue.TIMESTAMP
+  	});
+
+  	// newTrain.set({
+  	// 	trainName: trainName,
+  	// 	destination: destination,
+  	// 	startTime: startTime,
+  	// 	frequency: frequency,
+  	// 	dateAdded: firebase.database.ServerValue.TIMESTAMP
+  	// });
+
+  });
+
+  database.ref().on("child_added", function(childSnapshot) {
+  	trainName = childSnapshot.val().trainName;
+  	destination = childSnapshot.val().destination;
+  	startTime = childSnapshot.val().startTime;
+  	frequency = childSnapshot.val().frequency;
+  })
